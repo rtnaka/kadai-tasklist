252dd98b890493394223d7ae99aeec1f5eabeedc
rtnaka <rqotnc@gmail.com>
2021-10-21 23:16:02 +0900

    ページネーションを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -34,10 +34,29 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
+
+        // 開くページ数を取得（デフォルトは1ページ目）
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) {}
+
+
+        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class)
+                .setFirstResult(15 * (page - 1))
+                .setMaxResults(15)
+                .getResultList();
+
+        long tasks_count = (long)em.createNamedQuery("getTasksCount", Long.class)
+                                        .getSingleResult();
+
+
+
         em.close();
 
         request.setAttribute("tasks", tasks);
+        request.setAttribute("tasks_count", tasks_count);
+        request.setAttribute("page", page);
 
         // フラッシュメッセージがセッションスコープにセットされていたら
         // リクエストスコープに保存する（セッションスコープからは削除）
